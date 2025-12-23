e63f83c775ce490f9c7884d31209c7ba92e5371c
jeongmin-03 <jj.minn333@gmail.com>
2025-12-23 18:52:42 +0900

    12월 2,4주차 20243163 진정민
    Fixes the input parsing and sorting of digits in the main method.

--- Dec_2+4_jeongmin.java
+++ Dec_2+4_jeongmin.java
@@ -76,3 +76,33 @@ public class Main {
         bw.close();  
     }
 }
+
+#1427
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+       
+        String num = br.readLine();
+        //ArrayList<int> list = new ArrayList<int>(); error!! int =>xxxx
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        
+        for(int i =0;i<num.length();i++) {
+        	int n = Integer.parseInt(num.substring(i,i+1));
+        	list.add(n);
+        }
+        list.sort(Comparator.reverseOrder());
+        
+        for (int number : list) {
+        	bw.write(number+"");
+        }
+        
+        
+        bw.flush(); 
+        bw.close();  
+    }
+}
