2314b60a0bd19d45032a5063590ffc8063adec6e
jeongmin-03 <jj.minn333@gmail.com>
2026-01-05 00:56:33 +0900

    1월 1주차 20243163 진정민
    Added logic to calculate combinations using BigInteger.

--- JAN_1_jeongmin.java
+++ JAN_1_jeongmin.java
@@ -1,3 +1,51 @@
+#1010
+
+import java.io.*;
+import java.util.*;
+import java.math.BigInteger;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+	        int num = Integer.parseInt(br.readLine());
+	        
+	        for(int k=0;k<num;k++) {
+	        	
+	        	String[] tokens = br.readLine().split(" ");
+	        	int n = Integer.parseInt(tokens[0]);
+	        	int m = Integer.parseInt(tokens[1]);
+	        	
+	        	int a = m - n;
+	        	
+	        	BigInteger sum_m = BigInteger.ONE;
+	        	BigInteger sum_n = BigInteger.ONE;
+	        	BigInteger sum_a = BigInteger.ONE;
+	        	
+	        	for (int i = m; i > 0; i--) {
+	        		sum_m = sum_m.multiply(BigInteger.valueOf(i));
+	        	}
+	        	
+	        	for (int i = n; i > 0; i--) {
+	        		sum_n = sum_n.multiply(BigInteger.valueOf(i));
+	        	}
+	        	
+	        	for (int i = a; i > 0; i--) {
+	        		sum_a = sum_a.multiply(BigInteger.valueOf(i));
+	        	}
+	        	
+	        	BigInteger C = sum_m.divide(sum_n.multiply(sum_a));
+	        	
+	        	bw.write(C.toString() + "\n");
+	        }
+
+	        bw.flush();
+	        bw.close();
+    }
+}
+
+
 #10815
 
 public class Main {
