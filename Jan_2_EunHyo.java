9a51cb5d87e6e96afbc8659a8b712a8639ca9258
HYoMo-nim <phj61004@gmail.com>
2026-01-07 19:54:36 +0900

    1월 2주차 20233277 김은효

--- Jan_2_EunHyo.java
+++ Jan_2_EunHyo.java
@@ -122,15 +122,90 @@ public class set_S {
 
 
 // No. 7785    회사에 있는 사람
-
+public class Company_employee {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int a=Integer.parseInt(br.readLine());
+		HashSet<String> employee=new HashSet<>();
+		
+		for(int i=0;i<a;i++) {
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			String name=st.nextToken();
+			String status=st.nextToken();
+			
+			if(status.equals("enter")) employee.add(name);
+			else  employee.remove(name);
+		}
+		
+		List<String> list=new ArrayList<>(employee);
+		Collections.sort(list, Collections.reverseOrder());
+		
+		for(String x:list) {
+			bw.write(x+"\n");
+		}
+		
+		bw.flush();
+		br.close();
+		bw.close();
+	}
+}
 
 
 // No. 1094    막대기
-
+public class Stick {
+	public static int cutting(int X, int have) {
+		int result=0;
+		if(X<have) return cutting(X, have/2);
+		else {
+			result+=1;
+			if(X==have) return result;
+			else if(X==have+have/2) return (result+1); 
+			else return result+cutting(X-have, have);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int X=sc.nextInt();
+		int have=64;
+		int result=cutting(X, have);
+		
+		System.out.println(result);
+	}
+}
 
 
 // No. 1476    날짜 계산
-
+public class Year_count {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+	
+		int year=0;
+		
+		int E=1;
+		int S=1;
+		int M=1;
+		while(true) {
+			year++;
+			if(a==E&&b==S&&c==M) break;
+			
+			E++;
+			S++;
+			M++;
+			
+			if (E==16) E=1;
+			if (S==29) S=1;
+			if (M==20) M=1;
+		}
+		System.out.println(year);
+	}
+}
 
 
 // No. 9655    돌게임
@@ -138,7 +213,40 @@ public class set_S {
 
 
 // No. 17478  재귀함수가 뭔가요? 
+public class What_is {
+	static String under="";
+	public static void chatbot(int a) {
+		String s=under;
+		String one="\"재귀함수가 뭔가요?\"";
+		String two="\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어. ";
+		String three="마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지. ";
+		String four="그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"";
+		String mid="\"재귀함수는 자기 자신을 호출하는 함수라네\"";
+		String end="라고 답변하였지.";
+		
+		if (a==0) {
+			System.out.println(s+one);
+			System.out.println(s+mid);
+			System.out.println(s+end);
+			return;
+		}
+		System.out.println(s+one);
+		System.out.println(s+two);
+		System.out.println(s+three);
+		System.out.println(s+four);
+		under+="____";
+		chatbot(a-1);
+		System.out.println(s+end);
+	}
 
+	public static void main(String[] args)  {
+		Scanner sc=new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+		chatbot(x);
+	}
+}
 
 
 // No. 1439    뒤집기
