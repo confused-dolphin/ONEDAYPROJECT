c7dd0c8148c8ee04cd87876f146c524bb62b3517
jeongmin-03 <jj.minn333@gmail.com>
2026-01-11 02:51:24 +0900

    1월 2주차 20243163 진정민

--- JAN_2_jeongmin.java
+++ JAN_2_jeongmin.java
@@ -177,3 +177,101 @@ public class Main {
 	     bw.close();
     }
 }
+
+
+#1476
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	  
+	     String[] tokens = br.readLine().split(" ");
+ 		 
+ 		 
+	     int num_E = Integer.parseInt(tokens[0]);
+	     int num_S = Integer.parseInt(tokens[1]);
+	     int num_M = Integer.parseInt(tokens[2]);
+	     int i=1;
+	     
+	     while(true) {
+	    	if(((i - num_E)%15 == 0) && ((i - num_S)%28 == 0) && ((i - num_M)%19 == 0)) {
+	    		 bw.write(i+"\n");
+	    		 break;
+	    	}
+	    	i++;  	
+	     }
+	     bw.flush();
+	     bw.close();
+    }
+}
+
+#9655
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	  
+	    
+	     int num = Integer.parseInt(br.readLine());
+	     int name = 0;
+	     
+	     while(true) {
+	    	 
+	    	 if(num - 3 > 0) {
+	    		 num = num - 3;
+	    		 name++;
+	    	 }else if(num - 3 == 0 || num-1==0) {
+	    		 name++;
+	    		 break;
+	    	 }else if(num-3 < 0 && num-1 > 0) {
+	    		 num = num-1;
+	    		 name++;
+	    	 }
+	     }
+	     
+	     if(name % 2 == 1) {
+	    	 bw.write("SK");
+	     }else {
+	    	 bw.write("CY");
+	     }
+	     bw.flush();
+	     bw.close();
+    }
+}
+
+
+#17478
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	  
+	     System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+	     int num = Integer.parseInt(br.readLine());
+	     String s = "";
+	     int i = 4;
+	     re(num,s,i);
+    }
+	public static void re(int num, String s, int i) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		System.out.println(s+"\"재귀함수가 뭔가요?\"");
+		System.out.println(s+"\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n"+s+"마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n"+s+"그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		s = "____" + s; 
+		num--;
+		if(num!=0) {
+			re(num, s, i);
+		}
+		if(num==0) {
+			System.out.println(s+"\"재귀함수가 뭔가요?\"");
+			System.out.println(s+"\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+			System.out.println(s+"라고 답변하였지.");
+		}
+		s= s.substring(i,s.length());
+		i +=4;
+		System.out.println(s+"라고 답변하였지.");
+	}
+}
