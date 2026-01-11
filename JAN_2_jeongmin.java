ce6fff662a835f3c9ffeb5f3c65f6f3f418c1fce
jeongmin-03 <jj.minn333@gmail.com>
2026-01-11 12:24:59 +0900

    1월 2주차 20243163 진정민

--- JAN_2_jeongmin.java
+++ JAN_2_jeongmin.java
@@ -275,3 +275,46 @@ public class Main {
 		System.out.println(s+"라고 답변하였지.");
 	}
 }
+
+#1439
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	  
+	     String num = br.readLine();
+	     char before, now; 
+	     int count_0 = 0, count_1 = 0;
+	     
+	     
+	     for(int k=0;k<num.length()-1;k++) {
+	    	 before = num.charAt(k);
+		     now = num.charAt(k+1);
+	    	 if(before=='0') {
+	    		 if(now != before) {
+	    			 count_0++;
+	    		 }
+	    	 }else if(before == '1') {
+	    		 if(now != before) {
+	    			 count_1++;
+	    		 }
+	    	 }
+	    }
+	    
+	    char n = num.charAt(num.length()-1);
+
+		if(n == '1')
+			count_1++;
+		else if(n == '0')
+			count_0++;
+		 
+	     if(count_0>=count_1) {
+	    	 bw.write(count_1+"\n");
+	     }else {
+	    	 bw.write(count_0+"\n");
+	     }
+	     bw.flush();
+	     bw.close();
+    }
+}	
